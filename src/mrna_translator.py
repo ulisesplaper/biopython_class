4081bc91c7d8fd8ade7c19d23c2f4f53ae026b82
Ulises Plascencia <ulisesp@lcg.unam.mx>
2022-09-04 14:38:18 -0500

    Diseniar logica del programa y aniadir encabezado

---
+++ src/mrna_translator.py
@@ -0,0 +1,37 @@
+'''
+ NAME
+      mrna_translator.py
+VERSION
+        0.1.0
+AUTHOR
+        Victor Ulises Plascencia Perez
+DESCRIPTION
+       Traduce una secuencia de mRNA en una secuencia proteica
+USAGE
+        usage: py mrna_translator.py [-h] [--version] path
+         
+ARGUMENTS
+        positional arguments:
+         path                  path of the mRNA sequence fasta file
+
+        options:
+         -h, --help            show this help message and exit
+         --version             show program's version number and exit
+SEE ALSO
+GitHub link
+        https://github.com/ulisesplaper/python_class/blob/master/src/mrna_translator.py
+'''
+# Definir argumentos opcionales y posicionales
+
+# Leer los argumentos desde la terminal
+
+# Abrir el archivo que contiene la secuencia de mRNA, asignarlo a una variable y cerrarlo.
+
+# Crear un diccionario con el codigo genetico {codon:aminoacido}
+
+# Convertir la variable tipo string que contiene la seq. de mRNA en una lista de codones
+
+# Recorre la lista de codones y, con base en el diccionario, llenar una lista nueva
+# con la secuencia proteica.
+
+# Imprimir la secuencia proteica en pantalla o generar un archivo fasta
