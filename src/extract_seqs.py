9121654bcb574be7d68d1c9321bdd6c826c0362b
ulisesplaper <ulisesplaper>
2022-10-03 18:12:37 -0500

    Agregar encabezado y logica del programa

---
+++ src/extract_seqs.py
@@ -0,0 +1,37 @@
+'''
+ NAME
+      extract_seqs.py
+VERSION
+        4t1.0.0
+AUTHOR
+        Victor Ulises Plascencia Perez
+DESCRIPTION
+        Extrae la secuencias de un archivo fastq 
+        cuyas bases esten por arriba de un score
+        dado
+USAGE
+
+ARGUMENTS
+        
+SEE ALSO
+GitHub link
+
+'''
+# Importar los modulos necesarios
+
+
+# Definir argumentos opcionales y posicionales
+
+# Leer los argumentos desde la terminal
+
+# Funcion get_seq_umbral
+# Inicializar un contador
+# Abrir el archivo y recorrer cada uno de los SeqObj
+# Extraer los scores de cada SeqObj y determinar el mino
+# Si el minimo esta por arriba del umbral, escribir la
+# secuencia en un archivo e incrementar el contador
+
+# Imprimir el numero de secuencias que pasan el umbral
+# y un mensaje para el usuario
+
+# Llamar a la secuencia
