52d566455f6149c930cf1a24e32d92eb1becf60a
ulisesplaper <ulisesplaper>
2022-10-03 19:08:02 -0500

    Agregar logica y encabezado del programa

---
+++ src/genbank_info_extract.py
@@ -0,0 +1,34 @@
+'''
+ NAME
+      genbank_inf_extract.py
+VERSION
+        4t_2.1.0.0
+AUTHOR
+        Victor Ulises Plascencia Perez
+DESCRIPTION
+        Extrae informacion de archivos
+        tipo gb
+USAGE
+        usage: py genbank_info_extract.py path
+         
+ARGUMENTS
+        positional arguments:
+        path                  path of the fastq file
+
+        --version             show program's version number and exit
+SEE ALSO
+GitHub link
+        https://github.com/ulisesplaper/python_class/blob/master/src/genbank_info_extract.py
+
+'''
+# Importar los modulos necesarios
+
+
+# Definir argumentos opcionales y posicionales
+
+
+# Leer los argumentos desde la terminal
+
+# Recorrer cada SeqObj en el archivo
+# Extraer la informacion (fecha, organismo y pais)
+# Imprimir
