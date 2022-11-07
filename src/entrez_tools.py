99023e2a471736c395151b734c7c36c5ac9b21ab
ulisesplaper <ulisesplaper>
2022-11-06 20:53:17 -0600

    Agregar encabezado y logica del programa

---
+++ src/entrez_tools.py
@@ -0,0 +1,55 @@
+'''
+ NAME
+      entrez_tools.py
+VERSION
+        6t1.0.0.
+AUTHOR
+        Victor Ulises Plascencia Perez
+DESCRIPTION
+        Funcion 1: genera el termino a buscar para las herramientas
+        del modulo Entrez dado un organismo y ciertos genes.
+        Funcion 2: Genera un diccionario con las bases de datos
+        y los ids de los documentos correspondientes a la base 
+        de datos dato un termino de busqueda
+USAGE
+ARGUMENTS
+        
+SEE ALSO
+GitHub link
+        https://github.com/ulisesplaper/python_class/blob/master/src/entrez_tools.py
+
+'''
+
+# Importar el modulo entrez
+
+
+# Definr argumentos opcionales y posicionales
+# Leer argumentos desde la terminal
+# Indicar e.mail
+
+
+# Funcion que genera el termino de busqueda
+
+# Recorrer el diccionario de organismos y genes
+# Agregar el nombre del organismo al termino
+# Agregar los nombres de los genes al termino
+# agrupandolos y usando el OL OR
+# Regresar el termino generado
+
+
+# Funcion get_ID_perDB
+# Buscar db que contienen informacion
+# del termino dado
+
+# Crear diccionario vacio
+
+# Recorrer la lista de resultados
+# de egquery y extraer las bases de datos y los conteos
+# de los match encontrados
+# Excluir 0s y Errores
+# Por cada base de datos extraer la lista de IDs
+# Agregar la db y los ids a un diccionario
+
+# Regresar el diccionario
+
+# Llamar a las funciones
