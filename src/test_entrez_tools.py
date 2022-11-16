4d1ef88e560c74e6b541b3fbd58d4a4302047bfb
ulisesplaper <ulisesplaper>
2022-11-15 18:38:38 -0600

    agregar logica y encabezado del programa

---
+++ src/test_entrez_tools.py
@@ -0,0 +1,36 @@
+'''
+ NAME
+      test_entrez_tools.py
+VERSION
+        7.2t1.0.0.
+AUTHOR
+        Victor Ulises Plascencia Perez
+DESCRIPTION
+        Genera un diccionario con las bases de datos
+        y los ids de los documentos correspondientes a la base 
+        de datos dato un termino de busqueda generado automaticamente
+USAGE
+       python test_entrez_tools.py 'organism1:gene1,gene2;organism2:gene1,gene2;...'
+         
+ARGUMENTS
+     info     information separating organism from genes by ':' genes by ','
+              and organisms by ';' Example:
+              organism1:gene1,gene2;organism2:gene1,gene2
+        
+SEE ALSO
+GitHub link
+        https://github.com/ulisesplaper/python_class/blob/master/src/test_entrez_tools.py
+'''
+
+# Importar los modulos necesarios
+
+# Definir argumentos opcionales y posicionales
+
+# Leer los argumentos desde la terminal
+
+# Indicar e.mail
+
+# Definir excepciones del programa
+
+# Llamar a las funciones del modulo entrez_tools_module.py
+
